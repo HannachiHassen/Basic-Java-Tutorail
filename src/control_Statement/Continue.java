436b64bfe0fefe5fce312ef0d176c45e877d2817
HannachiHassen <hannachihassen07@gmail.com>
2021-09-16 14:57:31 -0400

    create control statement and OOP folder

---
+++ src/control_Statement/Continue.java
@@ -0,0 +1,13 @@
+package control_Statement;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		for (int j = 0; j <= 6; j++) {
+			if (j == 4) {
+				continue;
+			}
+			System.out.print(j + " ");
+		}
+	}
+}
---
+++ src/control_Statement/While_Loop.java
@@ -0,0 +1,12 @@
+package control_Statement;
+
+public class While_Loop {
+
+	public static void main(String[] args) {
+		int i = 10;
+		while (i > 1) {
+			System.out.println(i);
+			i--;
+		}
+	}
+}
